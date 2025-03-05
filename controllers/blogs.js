a06a202bb028ecd24be32c6f477fe5a3cf9f566f
arenasesteban <esteban.arenas.a@usach.cl>
2025-03-05 16:39:36 -0300

    [Esteban] Add exercise 17 for Part 4

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,23 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (req, res) => {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
     res.json(blogs);
 });
 
 blogsRouter.post('/', async (req, res) => {
+    const { title, author, url, likes, userId } = req.body;
+
+    const user = await User.findById(userId);
+
     const blog = new Blog({ 
-        ...req.body,
-        likes: req.body.likes || 0
+        title,
+        author,
+        url,
+        likes: likes || 0,
+        user: user.id
     });
 
     if(!req.body.title || !req.body.url) {
@@ -17,6 +25,10 @@ blogsRouter.post('/', async (req, res) => {
     }
 
     const savedBlog = await blog.save();
+
+    user.blogs = [...user.blogs, savedBlog.id];
+    await user.save();
+
     res.status(201).json(savedBlog);
 });
 
